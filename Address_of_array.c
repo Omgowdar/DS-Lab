eee3f43c17b6c94ea11d7f5b4cde0d4a3d21f030
Omgowdar <ggowdar.cs24@bmsce.ac.in>
2025-09-23 09:38:35 +0530

    Update Address_of_array.c

--- Address_of_array.c
+++ Address_of_array.c
@@ -6,8 +6,9 @@ int main() {
 
     printf("Characters and their addresses:\n");
     for (i = 0; arr[i] != '\0'; i++) {
-        printf("arr[%d] = %c Address = %p\n", i, arr[i], (void*)&arr[i]);
+        printf("arr[%d] = %c Address = %p\n", i, arr[i], &arr[i]);
     }
 
     return 0;
+
 }
