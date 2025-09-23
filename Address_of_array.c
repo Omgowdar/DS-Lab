5126fc47d254dafa04e9a5888fd0f58d385a8351
Omgowdar <ggowdar.cs24@bmsce.ac.in>
2025-09-23 09:27:25 +0530

    Add files via upload

---
+++ Address_of_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char arr[] = "Hello"; // character array
+    int i;
+
+    printf("Characters and their addresses:\n");
+    for (i = 0; arr[i] != '\0'; i++) {
+        printf("arr[%d] = %c Address = %p\n", i, arr[i], (void*)&arr[i]);
+    }
+
+    return 0;
+}
